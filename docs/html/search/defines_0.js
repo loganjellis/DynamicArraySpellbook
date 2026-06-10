82c6f705350609dbaf745a39848d728b4e22b089
itsLoJo <itslogan230@gmail.com>
2026-06-09 22:13:56 -0400

    added docs

---
+++ docs/html/search/defines_0.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['daspllbk_5fadd_0',['daspllbk_add',['../d2/d29/daspllbk_8h.html#af90a41db63ce91320f25aca09fc8c3be',1,'daspllbk.h']]],
+  ['daspllbk_5fapi_1',['DASPLLBK_API',['../d2/d29/daspllbk_8h.html#a316366c0fb993fa0ac9959411f1fb9e0',1,'daspllbk.h']]],
+  ['daspllbk_5fexpand_2',['daspllbk_expand',['../d2/d29/daspllbk_8h.html#aabca71dcdf1d674a512ff1aebccfafe6',1,'daspllbk.h']]],
+  ['daspllbk_5ffree_3',['daspllbk_free',['../d2/d29/daspllbk_8h.html#a14814d1c25d35960a2a3853a647fc0b0',1,'daspllbk.h']]],
+  ['daspllbk_5fget_4',['daspllbk_get',['../d2/d29/daspllbk_8h.html#af261faf7c81781e47a78a010b15a7a92',1,'daspllbk.h']]],
+  ['daspllbk_5finit_5',['daspllbk_init',['../d2/d29/daspllbk_8h.html#ac124298e50f9675898d2c3d805d64601',1,'daspllbk.h']]],
+  ['daspllbk_5finsert_6',['daspllbk_insert',['../d2/d29/daspllbk_8h.html#ae579441542fe650f73559110cce1c4b6',1,'daspllbk.h']]],
+  ['daspllbk_5fremove_7',['daspllbk_remove',['../d2/d29/daspllbk_8h.html#a1f1bf6709f8aee1b3015ff2fe91ed38e',1,'daspllbk.h']]],
+  ['daspllbk_5fset_8',['daspllbk_set',['../d2/d29/daspllbk_8h.html#a270f08c4510f14fca6767a66f5c0f1ce',1,'daspllbk.h']]]
+];
